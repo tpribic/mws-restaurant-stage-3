0589d521b5ec8b02a9ffc0f03b3d969020a1359e
tpribic <tomi.pribic@gmail.com>
2018-08-10 15:37:11 +0200

    SW registration as function in main.js, removed old json db from caching

--- restaurant/js/main.js
+++ restaurant/js/main.js
@@ -11,7 +11,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
   fetchNeighborhoods();
   fetchCuisines();
   fetchReviews();
+  registerServiceWorker();
 });
+
+registerServiceWorker = () => {
+  if (navigator.serviceWorker) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('sw.js')
+        .then(registration => console.log('Service Worker registered!'))
+        .catch(err => console.log('ServiceWorker registration failed: ', err));
+    })
+  }
+}
 
 fetchReviews = () => {
   DBHelper.fetchReviews((error, reviews) => {
